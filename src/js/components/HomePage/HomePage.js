fe8dc19f348de2c242ba8eb1b667ef1962c790e5
Evodio Garcia <evodiog@gmail.com>
2020-06-04 23:08:24 -0700

    adds loader for imgs on projects and some css changes wip

--- src/js/components/HomePage/HomePage.js
+++ src/js/components/HomePage/HomePage.js
@@ -11,27 +11,46 @@ const HomeWrapper = styled.section`
   }
 `;
 
-const projectHolder = projects.reverse().map(project => (
-  <div className="project" key={project.id}>
-    <a href={project.href} target='blank' alt={project.alt}>
-      <img src={project.img} alt={project.alt} />
-    </a>
-    <div className='project-info'>
-        <h3>{project.title}</h3>
-      <p>{project.description}</p>
-      <a href={project.href} target='blank' alt={project.alt}><span>Check it out</span></a>
-    </div>
-  </div>
-));
+// const projectHolder = projects.reverse().map(project => (
+//   <div className="project" key={project.id}>
+//     <a href={project.href} target='blank' alt={project.alt}>
+//       <img src={project.img} alt={project.alt} />
+//     </a>
+//     <div className='project-info'>
+//         <h3>{project.title}</h3>
+//       <p>{project.description}</p>
+//       <a href={project.href} target='blank' alt={project.alt}><span>Check it out</span></a>
+//     </div>
+//   </div>
+// ));
 
 
 function HomePage() {
+  const [loader, setLoader] = useState(true);
+
+  useEffect(()=> {
+    setLoader(false);
+  }, []);
+
   return (
     <HomeWrapper>
       <div className="row">
-      <h3>Projects</h3>
-        {projectHolder}
-      <h3>More Coming Soon</h3>
+        <h3>Projects</h3>
+        {
+          projects.reverse().map(project => (
+            <div className="project" key={project.id}>
+              <a href={project.href} target='blank' alt={project.alt}>
+                {loader ? <div id='loader'></div> : <img src={project.img} alt={project.alt} />}
+              </a>
+              <div className='project-info'>
+                <h3>{project.title}</h3>
+                <p>{project.description}</p>
+                <a href={project.href} target='blank' alt={project.alt}><span>Check it out</span></a>
+              </div>
+            </div>
+          ))
+        }
+        <h3>More Coming Soon</h3>
       </div>
     </HomeWrapper>
   )
