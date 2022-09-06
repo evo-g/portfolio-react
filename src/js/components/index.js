d7a551b936dca81ae8ea627d042b7cbf1e6d41c6
Evo <evo.garcia@newrocket.com>
2022-09-05 19:53:32 -0700

    changes to folder structure

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,7 +1,4 @@
 export { default as Navbar } from './Navbar';
-export { default as AboutPage  } from './AboutPage';
-export { default as ContactPage } from './ContactPage';
 export { default as Footer } from './Footer';
-export { default as HomePage } from './HomePage';
-export { default as ServicesPage } from './ServicesPage';
-export { default as DefaultPage } from './DefaultPage';
+
+
